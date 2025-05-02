44742050e39da00c9bad7661bac9742a0ffc6ac4
Alex Jolley <itisjolley+github@gmail.com>
2025-05-01 19:10:25 -0600

    cleaned up Button component lines

--- src/components/Adder.jsx
+++ src/components/Adder.jsx
@@ -8,15 +8,26 @@ import { useState } from "react"
 // EXPORT
 export default function Adder() {
     return (            
-
         <div className="adder shadow">
             <AdderInput />
             
             <div className="adder-action-bar">
-                <Button variant="icon-only" icon={<ClockAlert size={16}/>} id="critical-button" />
-                <Button variant="icon-only" icon={<Shell size={16}/>} id="backburner-button" />
-                <Button variant="icon-only" icon={<Calendar size={16}/>} id="scheduled-button" />
-                <Button type="submit" variant="icon" icon={<Plus size={20}/>} id="adder-button" />
+                <Button variant="icon-only"
+                icon={<ClockAlert size={16}/>}
+                id="critical-button" />
+
+                <Button variant="icon-only"
+                icon={<Shell size={16}/>}
+                id="backburner-button" />
+
+                <Button variant="icon-only"
+                icon={<Calendar size={16}/>}
+                id="scheduled-button" />
+
+                <Button type="submit"
+                variant="icon"
+                icon={<Plus size={20}/>}
+                id="adder-button" />
             </div>
         </div>
     
